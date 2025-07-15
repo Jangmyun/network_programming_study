f02a9365623b2adfdbba559d38315ac8c8445962
Jangmyun <cndlsrb2739@gmail.com>
2025-07-16 08:44:54 +0900

    chore

--- self/echo_example/echo_server.c
+++ self/echo_example/echo_server.c
@@ -33,12 +33,12 @@ int main(int argc, char *argv[]) {
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   serv_addr.sin_port = htons(atoi(argv[1]));
 
-  if(bind(serv_sock, (sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
+  if(bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
     error_handling("bind() error");
   }
 
   if(listen(serv_sock, 5) == -1){
-    error_handling("listen() error");s
+    error_handling("listen() error");
   }
 
   clnt_addr_size = sizeof(clnt_addr);
