4646cda34e43397a987458181429b8d8fd53b8dc
Jangmyun <cndlsrb2739@gmail.com>
2025-07-16 17:22:24 +0900

    현재 디렉토리의 파일 개수 카운트 함수

--- hw1/serv.c
+++ hw1/serv.c
@@ -7,16 +7,15 @@
 #include <sys/stat.h>
 
 #define BUF_SIZE 1024
-// #define DEBUG
 
 void error_handling(char *message);
-int count_file(DIR *directory);
+int count_files(char *path, int *filename_size);
 
 int main(int argc, char *argv[]) {
   int server_socket = 0;
   int client_socket = 0;
   char buf[BUF_SIZE];
-  int str_len;
+  int str_len = 0;
 
   DIR *directory = NULL;
   struct dirent *entry = NULL;
@@ -43,4 +42,30 @@ void error_handling(char *message) {
   fputs(message, stderr);
   fputc('\n', stderr);
   exit(1);
+}
+
+/*
+path 디렉토리의 파일 개수를 카운트하고, filename_size
+
+return:
+*/
+int count_files(char *path, int *filename_size) {
+  DIR *dir = NULL;
+  struct dirent *entry;
+
+  if (path == NULL) return -1;
+  if (*filename_size != 0) return -1;
+  if ((dir = opendir(path)) == NULL) return -1;
+
+  int count = 0;
+
+  while ((entry = readdir(dir))) {
+    // file이면 count++
+    if (entry->d_type == DT_REG) {
+      count++;
+      *filename_size += entry->d_namlen;
+    }
+  }
+
+  return count;
 }
