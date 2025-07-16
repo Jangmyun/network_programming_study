4b36b152d550fca1231a6309971f7ee7e5109bea
Jangmyun <cndlsrb2739@gmail.com>
2025-07-16 21:26:59 +0900

    socket logic

--- hw1/clnt.c
+++ hw1/clnt.c
@@ -6,12 +6,37 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 // #define DEBUG
 
 void error_handling(char *message);
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  int sock = 0;
+  char buf[BUF_SIZE];
+  int read_cnt = 0;
+  struct sockaddr_in serv_addr;
+
+  if (argc != 3) {
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_addr.sin_port = htons(atoi(argv[2]));
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+    perror("connect() error");
+    exit(1);
+  }
+
+  close(sock);
+  return 0;
+}
 
 void error_handling(char *message) {
   fputs(message, stderr);
