04ecf22621b485b3a636362b856339ffc4e85ff8
Jangmyun <cndlsrb2739@gmail.com>
2025-07-17 02:44:43 +0900

    송수신 로직 while문 안으로 이동

--- hw1/clnt.c
+++ hw1/clnt.c
@@ -44,41 +44,58 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  read(sock, &filecount, sizeof(int));
-  if (filecount < 1) {
-    printf("filecount invalid\n");
-    close(sock);
-    exit(1);
-  }
-#ifdef DEBUG
-  printf("filecount = %d\n", filecount);
-#endif
-
-  int file_selected = 0;
-  long file_size = 0;
-  FileInfo fileinfo;
-  printf("\n<Read File Informations>\n");
-  for (int i = 0; i < filecount; i++) {
-    read_cnt = readn(sock, &fileinfo, sizeof(FileInfo));
-#ifdef DEBUG
-    printf("%d: %s | %ldbytes | FILEREAD:%d\n", i + 1, fileinfo.filename,
-           fileinfo.size, read_cnt);
-#endif
-    printf("%d: %s | %ldbytes\n", i + 1, fileinfo.filename, fileinfo.size);
-  }
-
+  // 다른 파일 계속해서 받을지 선택하며 반복
   while (1) {
-    fputs("Select file number to receive > ", stdout);
+    read(sock, &filecount, sizeof(int));
+    if (filecount < 1) {
+      printf("filecount invalid\n");
+      close(sock);
+      exit(1);
+    }
+#ifdef DEBUG
+    printf("filecount = %d\n", filecount);
+#endif
+
+    long file_size = 0;
+    FileInfo fileinfo;
+    printf("\n<Read File Informations>\n");
+    for (int i = 0; i < filecount; i++) {
+      read_cnt = readn(sock, &fileinfo, sizeof(FileInfo));
+#ifdef DEBUG
+      printf("%d: %s | %ldbytes | FILEREAD:%d\n", i + 1, fileinfo.filename,
+             fileinfo.size, read_cnt);
+#endif
+      printf("%d: %s | %ldbytes\n", i + 1, fileinfo.filename, fileinfo.size);
+    }
+
+    int file_selected = 0;
+
+    while (1) {
+      fputs("Select file number to receive > ", stdout);
+      fgets(buf, BUF_SIZE, stdin);
+      file_selected = atoi(buf);
+      if (file_selected > 0) {
+        break;
+      }
+      fputs("Input invalid.\n", stdout);
+    }
+    file_selected--;
+#ifdef DEBUG
+    printf("file_selected=%d\n", file_selected);
+#endif
+    writen(sock, &file_selected, sizeof(int));
+
+    // 계속 반복할지 입력받기
+    int cond = 1;
+    fputs("Keep going? (y / n) > ", stdout);
     fgets(buf, BUF_SIZE, stdin);
-    file_selected = atoi(buf);
-    if (file_selected > 0) {
+    if (strcmp(buf, "y")) {
+      cond = 0;
+      writen(sock, &cond, sizeof(int));
       break;
     }
-    fputs("Input invalid.\n", stdout);
-  }
-  file_selected--;
-  writen(sock, &file_selected, sizeof(int));
-  sleep(1);
+    writen(sock, &cond, sizeof(int));
+  }  // while
 
   close(sock);
   return 0;
