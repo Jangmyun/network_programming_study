30cbf45d5123a82a09537a3ff77df5989138b7d6
Jangmyun <cndlsrb2739@gmail.com>
2025-07-16 16:53:44 +0900

    [chore] lint

--- hw1/clnt.c
+++ hw1/clnt.c
@@ -1,20 +1,17 @@
+#include <arpa/inet.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <sys/stat.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 // #define DEBUG
 
 void error_handling(char *message);
 
-int main(int argc, char *argv[]){
-
-  return 0;
-}
+int main(int argc, char *argv[]) { return 0; }
 
 void error_handling(char *message) {
   fputs(message, stderr);
--- hw1/serv.c
+++ hw1/serv.c
@@ -1,10 +1,10 @@
+#include <arpa/inet.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <sys/stat.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 
 #define BUF_SIZE 1024
 // #define DEBUG
@@ -12,28 +12,28 @@
 void error_handling(char *message);
 int count_file(DIR *directory);
 
-int main() {
-	int server_socket = 0;
-	int client_socket = 0;
-	char buf[BUF_SIZE];
-	int str_len;
+int main(int argc, char *argv[]) {
+  int server_socket = 0;
+  int client_socket = 0;
+  char buf[BUF_SIZE];
+  int str_len;
 
-	DIR* directory = NULL;
-	struct dirent* entry = NULL;
-	struct stat st;
+  DIR *directory = NULL;
+  struct dirent *entry = NULL;
+  struct stat st;
 
-	int currBuffSize = 0;
+  int currBuffSize = 0;
 
-	directory = opendir(".");
-	if(directory == NULL){
-		perror("opendir() failed");
-		exit(1);
-	}
+  directory = opendir(".");
+  if (directory == NULL) {
+    perror("opendir() failed");
+    exit(1);
+  }
 
-	struct sockaddr_in serv_addr, clnt_addr;
-	socklen_t clnt_addr_size;
+  struct sockaddr_in serv_addr, clnt_addr;
+  socklen_t clnt_addr_size;
 
-	if(argc != 2){
+  if (argc != 2) {
     printf("Usage: %s <port>\n", argv[0]);
     exit(1);
   }
