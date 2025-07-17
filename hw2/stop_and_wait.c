4c1d1e6144fc852d74016b72381516e37bc0c159
Jangmyun <cndlsrb2739@gmail.com>
2025-07-18 05:19:54 +0900

    packet setter 함수

--- hw2/stop_and_wait.c
+++ hw2/stop_and_wait.c
@@ -10,3 +10,15 @@ int init_packet_header(pkt_t_h *ph) {
   return 0;
 }
 
+void set_packet_header(pkt_t_h *ph, unsigned short pkt_type, unsigned int seq,
+                       unsigned short ack, unsigned int data_size) {
+  ph->pkt_type = pkt_type;
+  ph->seq = seq;
+  ph->ack = ack;
+  ph->data_size = data_size;
+}
+void set_packet(pkt_t *p, pkt_t_h *ph, char *data) {
+  memcpy(p, ph, sizeof(*ph));
+  // ph만큼 pointer 이동 후 data에만 memcpy
+  memcpy(p + sizeof(*ph), data, ph->data_size);
+}
