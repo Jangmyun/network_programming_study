09ff4a43300f7beadc12c10f7c0bc4c49dffb12d
Jangmyun <cndlsrb2739@gmail.com>
2025-07-18 05:09:39 +0900

    pkt_t 헤더에 pkt_type을 통해 어떤 종류의 패킷인지 알 수 있도록 함

--- hw2/stop_and_wait.h
+++ hw2/stop_and_wait.h
@@ -3,6 +3,7 @@
 
 #include <arpa/inet.h>
 #include <dirent.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,9 +14,15 @@
 
 #define PKT_SIZE 512
 
-typedef struct {  // 12 bytes
+// pkt_type
+#define TYPE_DATA 0
+#define TYPE_ACK 1
+#define TYPE_CONNECTION_REQ 2
+
+typedef struct {
+  unsigned short pkt_type;  // data, ack, connection_req
   unsigned int seq;
-  unsigned int ack;
+  unsigned short ack;
   unsigned int data_size;
 } pkt_t_h;
 
@@ -25,5 +32,12 @@ typedef struct {
   pkt_t_h header;
   char data[PKT_DATA_SIZE];
 } pkt_t;
+
+int init_packet(pkt_t *p);
+int init_packet_header(pkt_t_h *ph);
+
+void set_packet_header(pkt_t_h *ph, unsigned int seq, unsigned short ack,
+                       unsigned short connect_req, unsigned int data_size);
+void set_packet(pkt_t *p, pkt_t_h *ph);
 
 #endif  // STOP_AND_WAIT_H
