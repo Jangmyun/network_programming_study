5ca3844acacabb009d55b1cb91cbe511102fb20c
Jangmyun <cndlsrb2739@gmail.com>
2025-07-18 12:25:41 +0900

    reliable_sento implementation

--- hw2/stop_and_wait.c
+++ hw2/stop_and_wait.c
@@ -34,3 +34,46 @@ void timeout(int sig) {
   }
 }
 
+ssize_t reliable_sendto(int sock, void *buff, size_t nbytes, int flags,
+                        struct sockaddr *to, struct sockaddr *connected_addr,
+                        unsigned int curr_seq) {
+  ssize_t read_len = 0;
+  pkt_t recv_pkt;
+
+  struct sockaddr_in from;
+  socklen_t from_len = sizeof(from);
+
+  int req_time = 0;
+  while (req_time < MAX_REQ) {
+    sendto(sock, buff, nbytes, flags, to, sizeof(*to));
+
+    ualarm(500000, 0);
+    timeout_flag = 0;
+
+    read_len = recvfrom(sock, &recv_pkt, PKT_SIZE, 0, (struct sockaddr *)&from,
+                        &from_len);
+    if (read_len == -1) {
+      perror("recvfrom() failed");
+      req_time++;
+      continue;
+    }
+
+    // connected 된 address와 recvfrom으로 받은 address가 다르면 무시
+    if (((struct sockaddr_in *)connected_addr)->sin_addr.s_addr !=
+        from.sin_addr.s_addr) {
+      req_time++;
+      continue;
+    }
+
+    // ack packet을 정상적으로 받았으면 읽은 사이즈 리턴
+    if (recv_pkt.header.pkt_type == TYPE_ACK && recv_pkt.header.ack == 1 &&
+        recv_pkt.header.seq == curr_seq) {
+      ualarm(0, 0);
+      timeout_flag = 0;
+      return read_len;
+    }
+  }
+
+  return -1;
+}
+
