3911caf1cfbd32fcd991cdc72e0a1d6baa1fa1ee
Jangmyun <cndlsrb2739@gmail.com>
2025-07-19 17:20:13 +0900

    server에 connection req 로직 구현

--- hw2_1/receiver.c
+++ hw2_1/receiver.c
@@ -1,7 +1,87 @@
 #include "rudp.h"
 
+int setConnectionInfo(ConnectionInfo *conn, char *ipArg, char *portArg);
+void connectWithServer(ConnectionInfo *conn);
+
 int main(int argc, char *argv[]) {
-  printf("");
+  ConnectionInfo conn;
+  char buf[PKT_DATA_SIZE];
+  int rw_len;
+
+  setConnectionInfo(&conn, argv[1], argv[2]);
+
+  struct sockaddr_in from_addr;
 
   return 0;
+}
+
+int setConnectionInfo(ConnectionInfo *conn, char *ipArg, char *portArg) {
+  conn->sock = socket(PF_INET, SOCK_DGRAM, 0);
+  if (conn->sock == -1) {
+    perror("socket() failed");
+    exit(1);
+  }
+
+  memset(&conn->addr, 0, sizeof(conn->addr));
+  conn->addr.sin_family = AF_INET;
+  conn->addr.sin_addr.s_addr = inet_addr(ipArg);
+  conn->addr.sin_port = htons(atoi(portArg));
+  return 0;
+}
+
+void connectWithServer(ConnectionInfo *conn) {
+  Packet sendPacket, recvPacket;
+  setPacketHeader(&sendPacket, PKT_CONNECTION_REQ, 0, 0, 0);
+
+  struct sockaddr_in from_addr;
+  socklen_t from_addr_len = sizeof(from_addr);
+
+  int rw_len = 0;
+  puts("Connect Request Sent ...");
+
+  int connectionTry = 0;
+  while (connectionTry < MAX_REQ) {
+    rw_len = sendto(conn->sock, &sendPacket, PKT_SIZE, 0,
+                    (struct sockaddr *)&conn->addr, conn->addr_len);
+    if (rw_len == -1) {
+      perror("sendto() failed");
+      connectionTry++;
+      continue;
+    }
+#ifdef DEBUG
+    puts("Request sent");
+#endif
+
+    ualarm(500000, 0);
+    timeoutFlag = 0;
+
+    rw_len = recvfrom(conn->sock, &recvPacket, PKT_SIZE, 0,
+                      (struct sockaddr *)&from_addr, &from_addr_len);
+    if (rw_len == -1) {
+      perror("read() failed");
+      connectionTry++;
+      continue;
+    }
+
+    if (conn->addr.sin_addr.s_addr != from_addr.sin_addr.s_addr) {
+      connectionTry++;
+      continue;
+    }
+
+    if (recvPacket.header.packetType == PKT_CONNECTION_REQ &&
+        recvPacket.header.ack == 1) {
+      ualarm(0, 0);
+      timeoutFlag = 0;
+      break;
+    }
+  }  // while
+
+  if (timeoutFlag == 1) {
+    puts("Connection failed");
+    close(conn->sock);
+    exit(1);
+  }
+
+  puts("Connection Established");
+  return;
 }
