96252560567b42b8b172e5f2ed8f376d260550a6
Jangmyun <cndlsrb2739@gmail.com>
2025-07-19 22:47:56 +0900

    reliable한 recvfrom 함수 작성

--- hw2_1/rudp.c
+++ hw2_1/rudp.c
@@ -76,9 +76,71 @@ int r_sendto(ConnectionInfo *conn, void *data_buff, unsigned int dataSize,
     }
 
     try++;
-  }
+  }  // while
 
   fprintf(stderr, "r_sendto() failed\n");
+  return -1;
+}
+
+int r_recvfrom(ConnectionInfo *conn, void *data_buff, unsigned int curr_seq) {
+  int rw_len = 0;
+
+  Packet recvPacket, ackPacket;
+  initPacket(&ackPacket);
+  setPacketHeader(&ackPacket, PKT_ACK, curr_seq, 1, 0);
+
+  struct timeval timeoutTime = {0, 500000};  // 0.5sec
+
+  struct sockaddr_in from_addr;
+  socklen_t from_addr_len = sizeof(from_addr);
+
+  if (setsockopt(conn->sock, SOL_SOCKET, SO_RCVTIMEO, &timeoutTime,
+                 sizeof(timeoutTime)) == -1) {
+    perror("setsockopt() failed");
+    return -1;
+  }
+
+  int try = 0;
+  while (try < MAX_REQ) {
+    rw_len = recvfrom(conn->sock, &recvPacket, PKT_SIZE, 0,
+                      (struct sockaddr *)&from_addr, &from_addr_len);
+    if (rw_len == -1) {
+      if (errno == EAGAIN) {
+        try++;
+        continue;
+      } else {
+        perror("recvfrom() failed");
+        return -1;
+      }
+    }
+
+    if (recvPacket.header.packetType == PKT_DATA) {
+      if (recvPacket.header.seq == curr_seq) {  // seq 같으면 데이터 복사
+        // data_buff에 packet data 복사
+        memcpy(data_buff, recvPacket.data, recvPacket.header.dataSize);
+
+        sendto(conn->sock, &ackPacket, PKT_SIZE, 0,
+               (struct sockaddr *)&conn->recv_addr, &conn->recv_addr_len);
+
+        timeoutTime.tv_usec = 0;
+        if (setsockopt(conn->sock, SOL_SOCKET, SO_RCVTIMEO, &timeoutTime,
+                       sizeof(timeoutTime)) == -1) {
+          perror("setsockopt() failed");
+          return -1;
+        }
+
+        return recvPacket.header.dataSize;
+      } else {  // seq 다르면 ack 재전송]
+        sendto(conn->sock, &ackPacket, PKT_SIZE, 0,
+               (struct sockaddr *)&conn->recv_addr, &conn->recv_addr_len);
+        continue;
+      }
+    }
+
+    try++;
+  }  // while
+
+  fprintf(stderr, "r_recvfrom() failed\n");
   return -1;
 }
 
