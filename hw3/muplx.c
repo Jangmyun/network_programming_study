4743d247520c162ec8003ef47fa16116a6a37189
Jangmyun <cndlsrb2739@gmail.com>
2025-07-21 00:12:35 +0900

    file 관련 함수 재작성

--- hw3/muplx.c
+++ hw3/muplx.c
@@ -46,3 +46,62 @@ ssize_t writen(int fd, const void *data, size_t n) {
 
   return n - left;
 }
+
+int count_files(char *path) {
+  DIR *dir = NULL;
+  struct dirent *entry;
+
+  if (path == NULL) return -1;
+  if ((dir = opendir(path)) == NULL) return -1;
+
+  int count = 0;
+
+  while ((entry = readdir(dir))) {
+    // file 혹은 디렉토리면 count증가
+    if (entry->d_type == DT_REG || entry->d_type == DT_DIR) {
+      count++;
+    }
+  }
+
+  closedir(dir);
+
+  return count;
+}
+
+FileInfo *read_files(char *path) {
+  int count = 0;
+  int filecount = count_files(path);
+  if (path == NULL || filecount < 1) exit(1);
+
+  FileInfo *fileinfos = (FileInfo *)malloc(sizeof(FileInfo) * filecount);
+  if (fileinfos == NULL) {
+    errorExit("malloc() failed");
+  }
+
+  DIR *dir = NULL;
+  struct dirent *entry;
+  struct stat curr_stat;
+
+  dir = opendir(path);
+  if (dir == NULL) {
+    errorExit("opendir() failed");
+  }
+
+  while ((entry = readdir(dir))) {
+    if (count == filecount) break;
+    if (entry->d_type == DT_REG || entry->d_type == DT_DIR) {
+      stat(entry->d_name, &curr_stat);
+      strncpy(fileinfos[count].filename, entry->d_name, MAX_FILENAME_SIZE);
+      fileinfos[count].filename[MAX_FILENAME_SIZE] = 0;
+      fileinfos[count].size = curr_stat.st_size;
+
+      int isDir = entry->d_type == DT_DIR ? 1 : 0;
+      fileinfos[count].isDir = isDir;
+
+      count++;
+    }
+  }
+
+  closedir(dir);
+  return fileinfos;
+}
--- hw3/muplx.h
+++ hw3/muplx.h
@@ -11,9 +11,20 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define BUF_SIZE 1024
+#define MAX_FILENAME_SIZE 256
+
+typedef struct {
+  char filename[MAX_FILENAME_SIZE + 1];
+  off_t size;
+  int isDir;
+} FileInfo;
+
 void errorExit(const char *err);
 
 ssize_t readn(int fd, void *data, size_t n);
 ssize_t writen(int fd, const void *data, size_t n);
 
+int count_files(char *path);
+FileInfo *readFiles(char *path);
 #endif
