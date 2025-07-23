1ca8c9a45994978ebe41ec63d464961e8b86b285
Jangmyun <cndlsrb2739@gmail.com>
2025-07-23 14:22:48 +0900

    Suffix Trie를 위한 자료구조, 메서드 정의

--- hw4/search.c
+++ hw4/search.c
@@ -1,1 +1,46 @@
 #include "search.h"
+
+Node* createNode() {
+  Node* node = (Node*)malloc(sizeof(Node));
+  node->isEndOfWord = 0;
+  for (int i = 0; i < CHAR_SIZE; i++) {
+    node->children[i] = NULL;
+  }
+  return node;
+}
+
+void insert(Node* root, const char* suffix) {
+  Node* current = root;
+  for (int i = 0; suffix[i] != '\0'; i++) {
+    int idx = suffix[i];
+    if (current->children[idx] == NULL) {
+      current->children[idx] = createNode();
+    }
+  }
+  current->isEndOfWord = 1;
+}
+
+void display(Node* root, char str[], int level) {
+  if (root->isEndOfWord) {
+    str[level] = '\0';
+    printf("%s\n", str);
+  }
+  for (int i = 0; i < CHAR_SIZE; i++) {
+    if (root->children[i] != NULL) {
+      str[level] = i + 'a';
+      display(root->children[i], str, level + 1);
+    }
+  }
+}
+
+int search(Node* root, const char* suffix) {
+  Node* current = root;
+  for (int i = 0; suffix[i] != '\0'; i++) {
+    int idx = suffix[i];
+    if (current->children[idx] == NULL) {
+      return 0;
+    }
+    current = current->children[idx];
+  }
+  return current->isEndOfWord;
+}
--- hw4/search.h
+++ hw4/search.h
@@ -2,5 +2,25 @@
 #define SEARCH_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#define CHAR_SIZE 256
+
+typedef struct {
+  struct Node* children[CHAR_SIZE];
+  uint8_t isEndOfWord;
+} Node;
+
+typedef struct {
+  char* searchWord;
+  Node* trieRoot;
+  int word_length;
+} Keyword;
+
+Node* createNode();
+void insert(Node* root, const char* suffix);
+void display(Node* root, char str[], int level);
+int search(Node* root, const char* suffix);
 
 #endif
