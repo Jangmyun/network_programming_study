30fc3c4b585d02406eec64550efb53da92ac5399
Jangmyun <cndlsrb2739@gmail.com>
2025-07-26 16:16:26 +0900

    server에서 board position random 생성

--- final_project/server.c
+++ final_project/server.c
@@ -1,7 +1,26 @@
 #include "console.h"
+#include "game.h"
+
+board_pos *boardPositions = NULL;
+u_int16_t boardCount = 200;
+GameInitInfo gameInitInfo;
 
 int main(int arc, char *argv[]) {
-  printf("Hello world!");
+  struct timeval gameTime = {60, 0};
+  setGameInitInfo(&gameInitInfo, 2, 1, 32, 200, boardPositions, gameTime);
 
+#ifdef DEBUG
+  puts("GameInitInfo Set");
+#endif
+
+  // random board position generation
+  boardPositions = generateBoardPosition(&gameInitInfo);
+
+#ifdef DEBUG
+  puts("Random Board Positions Generated");
+  printBoardPositions(boardPositions, gameInitInfo.boardCount);
+#endif
+
+  free(boardPositions);
   return 0;
 }
