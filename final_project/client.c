e2d134e3b265b56c3bafbea29fb0deaef1a833e5
Jangmyun <cndlsrb2739@gmail.com>
2025-07-27 02:08:49 +0900

    drawPlayer 함수 구현 (차후 플레이어 위치에 board 여부에 따라 색 다르게 처리)

--- final_project/client.c
+++ final_project/client.c
@@ -6,20 +6,25 @@
 u_int16_t *boardPositions = NULL;
 u_int16_t boardCount = 200;
 GameInitInfo gameInitInfo;
+board_bitarray board_pos_bitarray;
 board_bitarray board_status;
 
 int window_x;
 int window_y;
 
 u_int16_t playerPos;
+
 int setPlayerInitPos(u_int8_t id);
 void drawBoards();
+void drawPlayer();
 
 int main(int arc, char *argv[]) {
   struct timeval gameTime = {60, 0};
   setGameInitInfo(&gameInitInfo, 2, 1, 32, 200, boardPositions, gameTime);
   boardPositions = generateBoardPosition(&gameInitInfo);
   randomizeBoardColor(&board_status, boardCount);
+
+  playerPos = setPlayerInitPos(gameInitInfo.playerId);
 
   window_x = getWindowWidth();
   window_y = getWindowHeight();
@@ -33,6 +38,7 @@ int main(int arc, char *argv[]) {
 #endif
 
   drawBoards();
+  drawPlayer();
 
   sleep(5);
 
@@ -78,5 +84,22 @@ void drawBoards() {
     clearColor();
     fflush(stdout);
   }
+  UnlockDisplay();
+}
+
+void drawPlayer() {
+  LockDisplay();
+  Position p;
+  transPositionXY(&p, playerPos, gameInitInfo.gridSize);
+
+  // TODO: 플레이어가 위치한 칸이 레드, 블루인지에 따라 글자색을 다르게 출력
+  // TODO: 플레이어가 위치한 칸이 board인지 체크하기 위해 board_pos_bitarray를
+  // 유지
+
+  gotoxy(p.x, p.y);
+  setColor(COLOR_PLAYER);
+  printf("[]");
+  clearColor();
+  fflush(stdout);
   UnlockDisplay();
 }
