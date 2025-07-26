ce4151501aa6d90b9213fe562a01620434891a51
Jangmyun <cndlsrb2739@gmail.com>
2025-07-26 16:15:16 +0900

    GameInitInfo Setter

--- final_project/game.c
+++ final_project/game.c
@@ -1,1 +1,18 @@
+#include "game.h"#include "game.h"
 #include "game.h"
+
+#include "console.h"
+
+/* =====COMMON===== */
+void setGameInitInfo(GameInitInfo *gameInfo, u_int8_t playerCount,
+                     u_int8_t playerId, u_int8_t gridSize, u_int16_t boardCount,
+                     board_pos *boardPosition, struct timeval gameTime) {
+  gameInfo->playerCount = playerCount;
+  gameInfo->playerId = playerId;
+  gameInfo->gridSize = gridSize;
+  gameInfo->boardCount = boardCount;
+  gameInfo->boardPositions = boardPosition;
+  gameInfo->gameTime = gameTime;
+  return;
+}
+
