b1f3f705e39c24e36178c8f520a1d76a583813bd
Jangmyun <cndlsrb2739@gmail.com>
2025-07-27 17:41:27 +0900

    server argument 받는 로직 구현

--- final_project/server.c
+++ final_project/server.c
@@ -2,20 +2,47 @@
 #include "game.h"
 
 board_pos *boardPositions = NULL;
-u_int16_t boardCount = 200;
 GameInitInfo gameInitInfo;
-board_bitarray board_status;
+board_bitarray board_pos_bitarray;  // 어떤 위치가 board인지
+board_bitarray board_status;        // board들의 상태 (blue, red);
 
-int main(int arc, char *argv[]) {
-  struct timeval gameTime = {60, 0};
-  setGameInitInfo(&gameInitInfo, 2, 1, 32, 200, boardPositions, gameTime);
+int currPlayer = 0;
+
+int setOptions(int argc, char *argv[], int *playerCount, int *gridSize,
+               int *boardCount, int *gameSec, int *port);
+
+int main(int argc, char *argv[]) {
+  // default options
+  int port = 5123;
+  {
+    int playerCount = 2;
+    int gridSize = 32;
+    int boardCount = 200;
+    int gameSec = 60;
+
+    setOptions(argc, argv, &playerCount, &gridSize, &boardCount, &gameSec,
+               &port);
+
+    struct timeval gameTime = {gameSec, 0};
+    setGameInitInfo(&gameInitInfo, playerCount, currPlayer, gridSize,
+                    boardCount, boardPositions, gameTime);
+
+    if (port < 1024 || port > 65535) {
+      printError("Cannot use well-known port");
+      exit(1);
+    }
+
+    validateGameInfo(&gameInitInfo);
+  }
 
 #ifdef DEBUG
   puts("GameInitInfo Set");
+  printGameInfo(&gameInitInfo);
+  printf("PORT = %d\n", port);
 #endif
 
-  // random board position generation
-  boardPositions = generateBoardPosition(&gameInitInfo);
+  // 설정한 보드의 개수만큼 전체 grid 인덱스에서 보드의 위치를 랜덤하게 설정
+  boardPositions = generateBoardPosition(&gameInitInfo, &board_pos_bitarray);
 
 #ifdef DEBUG
   puts("Random Board Positions Generated");
@@ -23,15 +50,46 @@ int main(int arc, char *argv[]) {
   puts("");
 #endif
 
-  // board color randomization
-  randomizeBoardColor(&board_status, boardCount);
+  // 선택한 보드들 중 랜덤한 절반의 flag를 1로 설정
+  randomizeBoardColor(&board_status, gameInitInfo.boardCount);
 
 #ifdef DEBUG
   puts("Random Board Color Selected");
-  printfBoardStatus(&board_status, boardCount);
+  printfBoardStatus(&board_status, gameInitInfo.boardCount);
   puts("");
 #endif
 
   free(boardPositions);
+  return 0;
+}
+
+int setOptions(int argc, char *argv[], int *playerCount, int *gridSize,
+               int *boardCount, int *gameSec, int *port) {
+  int c = 0;
+  while ((c = getopt(argc, argv, "n:s:b:t:p:")) != -1) {
+    switch (c) {
+      case 'n':
+        *playerCount = atoi(optarg);
+        break;
+      case 's':
+        *gridSize = atoi(optarg);
+        break;
+      case 'b':
+        *boardCount = atoi(optarg);
+        break;
+      case 't':
+        *gameSec = atoi(optarg);
+        break;
+      case 'p':
+        *port = atoi(optarg);
+        break;
+      default:
+        fprintf(stderr,
+                "Usage %s -n <playerNum> -s <gridSize> -b <boardNum> -t "
+                "<gameTime> -p <port>\n",
+                argv[0]);
+        exit(1);
+    }
+  }
   return 0;
 }
